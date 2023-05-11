ec215f85fab2bf51a2767663b164fad8a3fc1ab4
fjarar <flavio.jara.rebolledo@gmail.com>
2023-05-11 13:32:16 -0400

    added some bootstrap to the list

--- src/components/pure/task.jsx
+++ src/components/pure/task.jsx
@@ -1,17 +1,32 @@
 import { Task } from "../../models/task.class";
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import '../../styles/task.scss';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import "../../styles/task.scss";
 
 const TaskComponent = ({ task }) => {
-    useEffect(() => {
-        console.log('Created Task')
-        return () => {
-            console.log(`Task: ${task.name} is going to unmount`)
-        };
-    }, [task]);
-    return (
-        <div>
+  useEffect(() => {
+    console.log("Created Task");
+    return () => {
+      console.log(`Task: ${task.name} is going to unmount`);
+    };
+  }, [task]);
+  return (
+    <tr className="fw-normal">
+      <th>
+        <span className="ms-2">{task.name}</span>
+      </th>
+      <td className="align-middle">
+        <span>{task.description}</span>
+      </td>
+      <td className="align-middle">
+        {/* TODO: sustituir por un badge */}
+        <span>{task.level}</span>
+      </td>
+      <td className="align-middle">
+        <span>{task.completed}</span>
+      </td>
+    </tr>
+    /* {/* <div>
             <h2 className="task-name">
                 Nombre: { task.name }
             </h2>
@@ -24,14 +39,12 @@ const TaskComponent = ({ task }) => {
             <h5>
                 This task is: { task.completed ? 'COMPLETED':'PENDING' }
             </h5>
-        </div>
-    );
+        </div> */
+  );
 };
-
 
 TaskComponent.propTypes = {
-    task: PropTypes.instanceOf(Task)
+  task: PropTypes.instanceOf(Task),
 };
-
 
 export default TaskComponent;
